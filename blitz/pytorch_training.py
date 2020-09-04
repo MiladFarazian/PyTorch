6e25fd377f4de66272103efc687fb5498503423e
Milad Farazian <mfarazia@ucsc.edu>
2020-09-03 17:36:49 -0700

    Update

--- blitz/pytorch_training.py
+++ blitz/pytorch_training.py
@@ -57,9 +57,9 @@ import torch.nn.functional as F
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.conv1 = nn.Conv2d(3, 12, 5)
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.conv2 = nn.Conv2d(12, 16, 5)
         self.fc1 = nn.Linear(16 * 5 * 5, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
@@ -216,7 +216,7 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 print(device)
 
-""" """
+"""cpu"""
 
 net.to(device)
 
